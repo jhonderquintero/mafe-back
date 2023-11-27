725de4caea68847f933070710a936b27a5d3cd35
mafebohorquezh <mafebohorquezh@gmail.com>
2023-11-26 23:52:18 -0300

    Comment forget password

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -3,9 +3,9 @@ import { db } from '../models'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { authConfig } from '../config/auth.config'
-import { sendEmail } from './sendEmail.controller'
+// import { sendEmail } from './sendEmail.controller'
 import { Request, Response } from 'express'
-import crypto from 'crypto'
+// import crypto from 'crypto'
 
 const User = db.users
 
