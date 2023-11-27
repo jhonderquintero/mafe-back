16115806d0e1358b3b0a89d87c6cdc22e0103c0f
mafebohorquezh <mafebohorquezh@gmail.com>
2023-11-26 22:56:00 -0300

    login endpoint

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { db } from '../models'
 import bcrypt from 'bcrypt'
-// import jwt from 'jsonwebtoken'
-// import { authConfig } from '../config/auth.config'
+import jwt from 'jsonwebtoken'
+import { authConfig } from '../config/auth.config'
 // import { sendEmail } from './sendEmail.controller'
 import { Request, Response } from 'express'
 
@@ -29,35 +29,34 @@ const userController = {
       // Save the user in the database
       await user.save()
 
-      res.status(201).json({ message: 'User registered successfully!' })
+      res.status(201).json({ user })
+    } catch (error: any) {
+      res.status(500).json({ message: error.message })
+    }
+  },
+    
+  login: async (req: Request, res: Response) => {
+    try {
+      // Find user by email
+      const user = await User.findOne({ email: req.body.email })
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' })
+      }
+
+      // Check if the password is correct
+      const isMatch = await bcrypt.compare(req.body.password, user.password)
+      if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid credentials' })
+      }
+
+      // Generate JWT token
+      const token = jwt.sign({ userId: user._id }, authConfig.jwtSecret, { expiresIn: '1h' })
+
+      res.status(200).json({ token })
     } catch (error: any) {
       res.status(500).json({ message: error.message })
     }
   }
-
-  // User login
-  // login: async (req, res) => {
-  //   try {
-  //     // Find user by email
-  //     const user = await User.findOne({ email: req.body.email })
-  //     if (!user) {
-  //       return res.status(401).json({ message: 'Invalid credentials' })
-  //     }
-
-  //     // Check if the password is correct
-  //     const isMatch = await bcrypt.compare(req.body.password, user.password)
-  //     if (!isMatch) {
-  //       return res.status(401).json({ message: 'Invalid credentials' })
-  //     }
-
-  //     // Generate JWT token
-  //     const token = jwt.sign({ userId: user._id }, authConfig.jwtSecret, { expiresIn: '1h' })
-
-  //     res.json({ token })
-  //   } catch (error: any) {
-  //     res.status(500).json({ message: error.message })
-  //   }
-  // },
 
   // // Get a user by ID
   // getUserById: async (req, res) => {
--- src/app/routes/user.routes.ts
+++ src/app/routes/user.routes.ts
@@ -8,7 +8,7 @@ export const userRoutes = express.Router()
 userRoutes.post('/register', userController.register)
 
 // POST request for User login.
-// router.post('/login', userController.login)
+userRoutes.post('/login', userController.login)
 
 // // GET request for a specific User by ID.
 // router.get('/:userId', userController.getUserById)
